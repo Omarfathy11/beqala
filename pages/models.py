5565fcdab4c27d787a2f19678a870802550d40c8
hady eslam <fathy11@gmail.com>
2022-08-30 13:19:47 +0200

    message

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import User
 
 
-class Addresses(models.Model):
+class Address(models.Model):
     governorate = models.CharField(max_length=20)
     city = models.CharField(max_length=10)
     street = models.CharField(30)
@@ -12,18 +12,18 @@ class Place(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(blank=False, unique=True)
     description = models.TextField(max_length=800)
-    address = models.ForeignKey(Addresses, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
     openingHours = models.TextField()
 
 
-class Reviews(models.Model):
+class Review(models.Model):
     review = models.TextField(max_length=1000)
     reviewer = models.ForeignKey(User)
     reviewer = models.ForeignKey(Place)
     date = models.DateTimeField(auto_now_add=True)
 
 
-class Rates(models.Model):
+class Rate(models.Model):
     rate = models.IntegerField(max_length=5)
     ratedTo = models.ForeignKey(Place)
 
@@ -50,3 +50,16 @@ class GroceryStore(Place):
 
 class Hotel(Place):
     pass
+
+class Backers(Place):
+    pass
+
+class ATM(Place):
+    pass
+
+class Gym(Place):
+    pass
+
+class PlayGrounds(Place):
+    pass
+
