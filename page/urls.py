65bc80ff0c06c5ab00914c31b488a5dc254a5c80
hady eslam <fathy11@gmail.com>
2022-09-07 17:57:11 +0200

    message

--- page/urls.py
+++ page/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
 from page import views
+from rest_framework.routers import DefaultRouter
 
+
+route = DefaultRouter()
+
+route.register(r'restaurants', views.RestaurantModelViewSet)
+route.register(r'groceries', views.RestaurantModelViewSet)
+route.register(r'medical-clinics', views.RestaurantModelViewSet)
 
 urlpatterns = [
-    path('places/', views.PlaceModelViewSet.as_view({'get': 'list'})),
-    path('restaurants/', views.RestaurantModelViewSet.as_view({'get': 'list'})),
-    path('groceries/', views.GroceryStoreModelViewSet.as_view({'get': 'list'})),
-    path('medical-clinics/', views.MedicalClinicModelViewSet.as_view({'get': 'list'})),
+    path('', include(route.urls)),
+    #path('restaurants/', views.RestaurantModelViewSet),
+    #path('groceries/', views.GroceryStoreModelViewSet),
+    #path('medical-clinics/', views.MedicalClinicModelViewSet),
     #path('resturants/<int:pk>', ),
     #path('groceries/<int:pk>', ),
     #path('medical-clinics/<int:pk>', ),
