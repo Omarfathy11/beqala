6d7c5f24194e0fb667730b1e0da76c6166b4ef08
Mahmoud Hefny <hefny4@gmail.com>
2022-09-17 01:36:05 +0200

    ready to integrate with FE

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username','first_name','last_name','email','password']
+        fields = ['username','first_name','last_name','email','password']
         extra_kwargs = {
             'password' : { 'write_only' : True}
         }
